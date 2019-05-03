2e90f264c8fa23705978a2d19844b95424dc1c35
Alexey <theArnkey@live.com>
2019-05-03 22:28:51 +1000

    new functitons

--- arkRebarDrawing/arkRebarDrawing/Form1.Designer.cs
+++ arkRebarDrawing/arkRebarDrawing/Form1.Designer.cs
@@ -31,6 +31,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -63,11 +68,60 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "по 1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "лист 1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 274);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Узел";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 241);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "25";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 303);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Новый узел";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(182, 173);
+            this.ClientSize = new System.Drawing.Size(182, 339);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -75,6 +129,7 @@
             this.Name = "Form1";
             this.Text = "arkRebar";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +138,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- arkRebarDrawing/arkRebarDrawing/Form1.cs
+++ arkRebarDrawing/arkRebarDrawing/Form1.cs
@@ -224,5 +224,244 @@ namespace arkRebarDrawing
 
         private void button2_Click(object sender, EventArgs e) => RebarDimension(0);
 
+        private void CreateDetailView()
+        {
+            tsd.DrawingHandler dh = new tsd.DrawingHandler();
+
+            ui.DrawingObjectSelector dos = dh.GetDrawingObjectSelector();
+            tsd.DrawingObjectEnumerator doe = dos.GetSelected();
+
+            double minX = double.MaxValue;
+            double maxX = double.MinValue;
+
+            double minY = double.MaxValue;
+            double maxY = double.MinValue;
+
+            double minZ = double.MaxValue;
+            double maxZ = double.MinValue;
+
+            tsd.View v = null;
+
+            ArrayList drParts = new ArrayList();
+
+            while (doe.MoveNext())
+            {
+                if (doe.Current is tsd.ReinforcementGroup)
+                {
+                    tsd.ReinforcementGroup p = doe.Current as tsd.ReinforcementGroup;
+                    v = p.GetView() as tsd.View;
+                    drParts.Add(doe.Current);
+
+                    WorkPlaneHandler wph = new Model().GetWorkPlaneHandler();
+                    TransformationPlane tp = wph.GetCurrentTransformationPlane();
+                    TransformationPlane tp0 = new TransformationPlane(v.DisplayCoordinateSystem);
+                    wph.SetCurrentTransformationPlane(tp0);
+
+
+                    ModelObject mobj = new Model().SelectModelObject(p.ModelIdentifier);
+
+                    Point min = null;
+                    Point max = null;
+
+                    if (mobj is RebarGroup)
+                    {
+                        min = (mobj as RebarGroup).GetSolid().MinimumPoint;
+                        max = (mobj as RebarGroup).GetSolid().MaximumPoint;
+                    }
+
+                    if (mobj is SingleRebar)
+                    {
+                        min = (mobj as SingleRebar).GetSolid().MinimumPoint;
+                        max = (mobj as SingleRebar).GetSolid().MaximumPoint;
+                    }
+
+                    minX = Math.Min(min.X, minX);
+                    maxX = Math.Max(min.X, maxX);
+                    minX = Math.Min(max.X, minX);
+                    maxX = Math.Max(max.X, maxX);
+
+                    minY = Math.Min(min.Y, minY);
+                    maxY = Math.Max(min.Y, maxY);
+                    minY = Math.Min(max.Y, minY);
+                    maxY = Math.Max(max.Y, maxY);
+
+                    minZ = Math.Min(min.Z, minZ);
+                    maxZ = Math.Max(min.Z, maxZ);
+                    minZ = Math.Min(max.Z, minZ);
+                    maxZ = Math.Max(max.Z, maxZ);
+
+                    wph.SetCurrentTransformationPlane(tp);
+                }
+            }
+
+            double scale = Convert.ToDouble(textBox3.Text);
+
+            Point minPoint = new Point(minX, minY, minZ);
+            Point maxPoint = new Point(maxX + scale, maxY + scale, maxZ + scale);
+            Point centerPoint = new Point((minX + maxX) * 0.5 - scale, (minY + maxY) * 0.5 - scale, (minZ + maxZ) * 0.5 - scale);
+            Point LabelPoint = new Point(maxPoint.X + 2 * scale, maxPoint.Y + 2 * scale, maxPoint.Z + 2 * scale);
+
+            AABB ab = new AABB(minPoint, maxPoint);
+
+
+            // tsd.ContainerView cv = dh.GetActiveDrawing().GetSheet();
+            //tsd.View nv = new tsd.View(cv, v.DisplayCoordinateSystem, v.DisplayCoordinateSystem, drParts);
+            //tsd.View nv = new tsd.View(cv, v.DisplayCoordinateSystem, v.DisplayCoordinateSystem, ab);
+            // nv.Attributes.Scale = 15;
+            // nv.Insert();
+            tsd.View dvm = null;
+            tsd.DetailMark dm = null;
+
+            tsd.View.ViewAttributes va = new tsd.View.ViewAttributes("Узел");
+            va.Scale = 15;
+            tsd.TextElement te = new tsd.TextElement("М ");
+            te.Font.Name = "GOST 2.304 type A";
+            te.Font.Height = 3.5;
+
+            tsd.PropertyElement pe = new tsd.PropertyElement(tsd.PropertyElement.PropertyElementType.DetailViewLabelMarkPropertyElementTypes.Scale());
+            pe.Font.Name = "GOST 2.304 type A";
+            pe.Font.Height = 3.5;
+            tsd.PropertyElement viewname = new tsd.PropertyElement(tsd.PropertyElement.PropertyElementType.ViewLabelMarkPropertyElementTypes.ViewName());
+            viewname.Font.Name = "GOST 2.304 type A";
+            viewname.Font.Height = 3.5;
+
+            va.TagsAttributes.TagA1.TagContent.Clear();
+            va.TagsAttributes.TagA2.TagContent.Clear();
+            va.MarkSymbolAttributes.Size = 12;
+            va.MarkSymbolAttributes.Shape = tsd.MarkSymbolShape.None;
+            va.MarkSymbolAttributes.LineLength = 12;
+            va.MarkSymbolAttributes.LineLengthType = tsd.View.LabelLineLengthType.Minimal;
+
+            tsd.TextElement vtext = new tsd.TextElement("Узел ");
+            vtext.Font.Name = "GOST 2.304 type A";
+            vtext.Font.Height = 3.5;
+
+            va.TagsAttributes.TagA1.TagContent.Add(vtext);
+            va.TagsAttributes.TagA1.TagContent.Add(viewname);
+            va.TagsAttributes.TagA2.TagContent.Add(te);
+            va.TagsAttributes.TagA2.TagContent.Add(pe);
+
+            
+
+            va.TagsAttributes.TagA1.Location = tsd.TagLocation.AboveLine;
+            //va.TagsAttributes.TagA1.Offset = new Vector(0, 5, 0);
+            va.TagsAttributes.TagA2.Location = tsd.TagLocation.BelowLine;
+
+            tsd.DetailMark.DetailMarkAttributes dma = new tsd.DetailMark.DetailMarkAttributes();
+
+            dma.MarkName = textBox1.Text;
+            dma.BoundaryShape = tsd.DetailMark.DetailMarkAttributes.DetailBoundaryShape.Rectangular;
+
+            dma.BoundingLine.Type = tsd.LineTypes.DashDoubleDot;
+            dma.BoundingLine.Color = tsd.DrawingColors.Yellow;
+
+            //tsd.TextElement te = new tsd.TextElement(textBox2.Text);
+            //te.Font.Name = "Gost 2.304 type A";
+            //te.Font.Height = 3.5;
+
+            dma.TagsAttributes.TagA2.TagContent.Clear();
+            //dma.TagsAttributes.TagA2.TagContent.Add(te);
+
+            tsd.View.CreateDetailView(v, centerPoint, maxPoint, LabelPoint, new Point(0, 0, 0), va, dma, out dvm, out dm);
+
+           // tsd.DetailMark sm = new tsd.DetailMark(v, CenterPoint, max, labelPoint);
+
+        }
+
+        private void circle(int axis)
+        {
+            tsd.DrawingHandler dh = new tsd.DrawingHandler();
+
+            ui.DrawingObjectSelector dos = dh.GetDrawingObjectSelector();
+            tsd.DrawingObjectEnumerator doe = dos.GetSelected();
+            while (doe.MoveNext())
+            {
+                if (doe.Current is tsd.ReinforcementGroup)
+                {
+                    tsd.ReinforcementGroup p = doe.Current as tsd.ReinforcementGroup;
+                    tsd.View v = p.GetView() as tsd.View;
+
+                    WorkPlaneHandler wph = new Model().GetWorkPlaneHandler();
+                    TransformationPlane tp = wph.GetCurrentTransformationPlane();
+                    TransformationPlane tp0 = new TransformationPlane(v.DisplayCoordinateSystem);
+                    wph.SetCurrentTransformationPlane(tp0);
+
+                    ModelObject mobj = new Model().SelectModelObject(p.ModelIdentifier);
+
+                    Point min = null;
+                    Point max = null;
+
+                    if (mobj is Part)
+                    {
+                        min = (mobj as Part).GetSolid().MinimumPoint;
+                        max = (mobj as Part).GetSolid().MaximumPoint;
+                    }
+
+                    if (mobj is RebarGroup)
+                    {
+                        min = (mobj as RebarGroup).GetSolid().MinimumPoint;
+                        max = (mobj as RebarGroup).GetSolid().MaximumPoint;
+                    }
+
+                    if (mobj is SingleRebar)
+                    {
+                        min = (mobj as SingleRebar).GetSolid().MinimumPoint;
+                        max = (mobj as SingleRebar).GetSolid().MaximumPoint;
+                    }
+
+                    //if (mobj is RebarGroup)
+                    try
+                    {
+                        double scale = Convert.ToDouble(textBox3.Text);
+
+                        Point labelPoint = new Point(min.X + 2*scale, min.Y + 2*scale, min.Z + 2 * scale);
+
+                        Point CenterPoint = new Point(
+                            (min.X + max.X) / 2 - scale,
+                            (min.Y + max.Y) / 2 - scale,
+                            (min.Z + max.Z) / 2 - scale);
+
+                        max = new Point(max.X + scale, max.Y + scale, max.Z + scale);
+
+                        tsd.DetailMark sm = new tsd.DetailMark(v, CenterPoint, max, labelPoint);
+
+                       // MessageBox.Show(sm.BoundaryPoint.ToString() + Environment.NewLine + sm.CenterPoint.ToString());
+
+                        sm.Attributes.MarkName = textBox1.Text;
+                        sm.Attributes.BoundaryShape = tsd.DetailMark.DetailMarkAttributes.DetailBoundaryShape.Rectangular;
+
+                        sm.Attributes.BoundingLine.Type = tsd.LineTypes.DashDoubleDot;
+                        sm.Attributes.BoundingLine.Color = tsd.DrawingColors.Yellow;
+
+                        tsd.TextElement te = new tsd.TextElement(textBox2.Text);
+                        te.Font.Name = "Gost 2.304 type A";
+                        te.Font.Height = 3.5;
+
+                        sm.Attributes.TagsAttributes.TagA2.TagContent.Clear();
+                        sm.Attributes.TagsAttributes.TagA2.TagContent.Add(te);
+
+                        sm.Insert();
+
+
+                    }
+                    catch (Exception ex){ MessageBox.Show(ex.ToString()); }
+                    finally
+                    {
+                        wph.SetCurrentTransformationPlane(tp);
+                    }
+                }
+            }
+            dh.GetActiveDrawing().CommitChanges();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            circle(1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CreateDetailView();
+        }
     }
 }
